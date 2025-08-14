868636eb1e7e37f9106375ec5175bd588ad7c3f9
pengyang-Li <lipengyang69@gmail.com>
2025-08-14 10:31:27 +0800

    Update Qwen_3.py

--- QWen/Qwen_3.py
+++ QWen/Qwen_3.py
@@ -18,7 +18,7 @@ def parse_args():
                         help='save the downloaded data')
     parser.add_argument('--cad_qa', type=str, default="/home/jupyter-lpy/project/target_detection/CAD_VQA/data/small_dataset.json",
                         help='save the downloaded data')
-    parser.add_argument('--api_key', type=str, default="sk-ljbdphromcnxchfgbcvaxvlcslukcaasqmqafddompcgxeun",
+    parser.add_argument('--api_key', type=str, default="sk-xxx",
                         help='Path to save the results')
     parser.add_argument('--output', type=str, default="/home/jupyter-lpy/project/target_detection/CAD_VQA/QWen/output/qwen_result3/",
                         help='Path to save the results')
